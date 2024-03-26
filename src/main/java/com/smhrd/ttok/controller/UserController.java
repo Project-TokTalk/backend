b9a12a4ef4348f538b6016052224fbce1b35fc63
sonseungah <disket0882@gmail.com>
2024-03-26 17:26:20 +0900

    Build,Chore : CorsConfig 삭제, WebMvcConfig 추가(CORS), UserController path 수정

--- src/main/java/com/smhrd/ttok/controller/UserController.java
+++ src/main/java/com/smhrd/ttok/controller/UserController.java
@@ -12,6 +12,9 @@ import com.smhrd.ttok.DTO.request.user.UserRegisterDTO;
 import com.smhrd.ttok.DTO.response.UserResponseDTO;
 import com.smhrd.ttok.service.UserService;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,7 +24,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/register")
+    @PostMapping("/signup")
     public ResponseEntity<UserResponseDTO> register(@RequestBody UserRegisterDTO userRegisterDTO) {
         UserResponseDTO successUser = userService.register(userRegisterDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(successUser);
@@ -36,6 +39,20 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials"); // 인증 실패
     }
 }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie : cookies) {
+                cookie.setMaxAge(0);
+                cookie.setPath("/login");   
+                response.addCookie(cookie);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("Logout successful!"); // 로그아웃 성공
+    }
 
     
 }
