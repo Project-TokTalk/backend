38bfd3ea8d6b369eb491336890620265b64d2a6e
loseahmil <lhs363447@gmail.com>
2024-04-08 11:20:06 +0900

    Feat: 로그인 성공 시 세션에 사용자 휴대폰 번호 저장

--- src/main/java/com/smhrd/ttok/controller/UserController.java
+++ src/main/java/com/smhrd/ttok/controller/UserController.java
@@ -2,6 +2,7 @@ package com.smhrd.ttok.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.smhrd.ttok.service.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -31,14 +33,26 @@ public class UserController {
     }
 
    @PostMapping("/login")
-   public ResponseEntity<String> login(@RequestBody UserLoginDTO loginDTO) {
+   public ResponseEntity<String> login(@RequestBody UserLoginDTO loginDTO, HttpSession session) {
     // 사용자 인증을 수행합니다.
     if (userService.authenticate(loginDTO)) {
+        // 로그인 성공 시 세션에 사용자 아이디 저장
+        session.setAttribute("phone", loginDTO.getPhone());
         return ResponseEntity.ok("Login successful!"); // 로그인 성공
     } else {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials"); // 인증 실패
     }
 }
+
+    @GetMapping("/getPhone")
+    public ResponseEntity<String> getPhone(HttpSession session) {
+        String phone = (String) session.getAttribute("phone");
+        if (phone != null) {
+            return ResponseEntity.ok(phone);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not logged in");
+        }
+    }
 
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
@@ -53,6 +67,5 @@ public class UserController {
         }
         return ResponseEntity.status(HttpStatus.OK).body("Logout successful!"); // 로그아웃 성공
     }
-
     
 }
