62475bc00574eb8c50e560525b87ed780576591d
sonseungah <disket0882@gmail.com>
2024-04-10 00:16:52 +0900

    Build : 백엔드 localhost -> 외부 ip로 수정

--- src/main/java/com/smhrd/ttok/security/WebMvcConfig.java
+++ src/main/java/com/smhrd/ttok/security/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://13.125.89.127:3000")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
     }
 
